e3433a40436898dee2fe7a3d1905a152c2a7a5e3
George Markoulakis <gmarkoulakis@georges-mbp.attlocal.net>
2016-06-27 21:37:50 -0700

    third commit

--- echoserver.py
+++ echoserver.py
@@ -130,7 +130,7 @@ def send_message(token, recipient, text):
 def send_receipt(token, recipient, text):
   """Send the message text to recipient with id recipient.
   """
-  city = findAddress(recipient)
+  city = str(findAddress(recipient))
 
   r = requests.post("https://graph.facebook.com/v2.6/me/messages",
     params={"access_token": token},
