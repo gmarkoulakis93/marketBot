cd0d5ce2ffe310d6dc9313e197238bc142f44934
George Markoulakis <gmarkoulakis@georges-mbp.attlocal.net>
2016-06-27 21:21:54 -0700

    third commit

--- csvRead.py
+++ csvRead.py
@@ -3,6 +3,6 @@ def findAddress(recipient):
 	with open('sender_file.csv') as csvfile:
 		reader = csv.DictReader(csvfile)
 		for row in reader:
-			if int(row['m_id'])==recipient:
+			if row['m_id']==recipient:
 				print (row['city'])
 			pass
