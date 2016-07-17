c6cddaae96cc27a8935e1262bdb886e8afa27b0e
George Markoulakis <georgemarkoulakis2015@u.northwestern.edu>
2016-07-17 14:36:57 -0700

    add removePunctuation

--- stripPunct.py
+++ stripPunct.py
@@ -1,15 +1,10 @@
 import string
 def noPunct(myList):
+	for pair in myList:
+		for item in pair:
+				item = removePunctuation(item)
+
+def removePunctuation(myString):
 	punct = set(string.punctuation)
-	x = 0
-	for item in myList[x][0]:
-		for i in punct:
-			if i in myList[x][0]:
-				myList[x][0] = myList[x][0].replace(i,"").strip()
-		x = x +1
-	x = 0
-	for item in myList[x][1]:
-		for i in punct:
-			if i in myList[x][1]:
-				myList[x][1] = myList[x][1].replace(i,"").strip()
-		x = x +1
+	myString = ''.join(c for c in myString if c not in punct)
+	return myString
