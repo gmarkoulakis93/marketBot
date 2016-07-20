a26eb5091ecc33e0979738b5a2c1355d888d0407
George Markoulakis <georgemarkoulakis2015@u.northwestern.edu>
2016-07-19 22:21:20 -0700

    receipt math

--- echoserver.py
+++ echoserver.py
@@ -166,7 +166,7 @@ def send_receipt(token, recipient, text, myDicts):
   n=0
   for d in myDicts:
       k = keys[n]
-      total[k] = d['price'] * d['quantity']
+      total[k] = d['price']
       n = n+1
   cost = sum(total.values())
   tax        = TAX_RATE * cost
