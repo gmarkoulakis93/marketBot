48cf55ee60b6ca98382714d876380eada2ab392c
George Markoulakis <georgemarkoulakis2015@u.northwestern.edu>
2016-08-20 17:13:26 -0700

    try out buttons with links

--- echoserver.py
+++ echoserver.py
@@ -417,19 +417,37 @@ def playWithButtons(token, recipient, text):
         "attachment":{
           "type":"template",
           "payload":{
-            "template_type":"button",
-            "text":"Want this? https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/7/005/085/231/20d3c36.jpg",
-            "buttons":[
-              {
-              "type":"web_url",
-              "url":"https://petersapparel.parseapp.com",
-              "title":"Show Website"
-              },
-              {
-              "type":"postback",
-              "title":"Start Chatting",
-              "payload":"testingButtons"
-              }
+            [
+              "template_type":"button",
+              "text":"Want this? https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/7/005/085/231/20d3c36.jpg",
+              "buttons":[
+                    {
+                    "type":"web_url",
+                    "url":"https://petersapparel.parseapp.com",
+                    "title":"Show Website"
+                    },
+                    {
+                    "type":"postback",
+                    "title":"Start Chatting",
+                    "payload":"testingButtons"
+                    }            
+                ]
+            ],
+            [
+              "template_type":"button",
+              "text":"Or this? https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/7/005/085/231/20d3c36.jpg",
+              "buttons":[
+                    {
+                    "type":"web_url",
+                    "url":"https://petersapparel.parseapp.com",
+                    "title":"Show Website"
+                    },
+                    {
+                    "type":"postback",
+                    "title":"Start Chatting",
+                    "payload":"testingButtons"
+                    }            
+                ]
             ]
           }
         }
